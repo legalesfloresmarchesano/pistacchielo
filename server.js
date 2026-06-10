90455191e76e423a4bcff45a53ab0ae782f0e476
legalesfloresmarchesano <estudiofme1@gmail.com>
2026-06-10 01:52:10 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+};
+
+http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') filePath = './index.html';
+  
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'text/plain';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+}).listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
